b44f2865462830cc8cb386a7985db0124bab91f4
bitcoremgv <bitcoremgv@users.noreply.github.com>
2017-09-05 23:15:02 +0000

    Add files via upload

---
+++ thepackage.C
@@ -0,0 +1,235 @@
+ ,
+
+the db package
+
+var dbs =
+
+ {
+
+
+  asks_ids:{ place :{ is_place { latLng :{ lng : is_lng, lat : is_lat   , _place :is is  _place , place_:is_place     ,  _:{iscurrenr},low_id:{ is_low_id   ,
+   checkers_ids_: is_checkers_id , reencrypted_asks_ids: is_reencrypted_asks_ids ,asks_track : is_track_integer ,
+   asks_level_ : is_asks_level_integer, asks_letter__: is_asks_     is_asks_letter Letter integer ,  ,
+((
+
+
+
+
+ ledgers : {
+
+
+
+
+wallets_ledger : {
+
+ wallets_id : reencryption_asks_ids
+,crypteds :{
+
+     at_: { at_integer:{ _of_ : {reencrypted_integer :  reencrypted_of_: { needed_integer: { needed_of_: { crypteds_integer:{  : {  : {
+crypteds
+decrypted__  _bunch_of_o_ab_and_one
+   ,cryptos_used : { to_crypted__: cryptos_used_id_to_crypted ,
+   to_crypted_minus: { to_crypted_minus_integer :__  cryptos_used_id_to_crypted_minus  ,, ,
+
+   decryption_ask_keys_ : {new String  sending_backs_keys) :{
+   reencrypted__ : reencrypted_binary_buffer , keys_for_the_crypted_not_   the_reencrypted__ :      } } },  , instance_of_wallets_id_that_has_checkers_id__     :      , lowdollars__:{} },
+
+ } ,
+ checkers_ledger : {
+,.
+ asks_checks:{ isplain_  : the_things_that_is ,
+   instanceofdb : checks_instance_     of_dbs_integer:   __ :  instance_of_dbs  ,
+   checks_ids__ : asks_ids ,
+   checks__: send_backs_sorted },,
+
+
+constancy_ledger : {
+
+constancy_asks_id_ : asks_ids ,
+co}stancy_checks_ : send_backs_sorted ,
+unconstancy_issue__ : issue ,
+constancy_asks:{ isplain__: the_things_that_is , instanceofdbs:      _asks_integer__:  instance_of_dbs } },
+
+rewards_asks_ledger : { rewards_asks_id_ : asks_ids , checkers__ : checkers_ids , rewards_asked__ : checker_asked_rewards_integer },
+mean_a,.ed_rewards_ :{mean_ask_id_ : asks_ids , mean_asked_rewards__: mean_asked_rewards_integer } } },
+
+
+
+ used_guides:{
+checkers_used_guide : {	checkers_used:{     ,checkers_ids__: checkers_id , material_characteristics__: material_charecharacteristics  , wallets__: wallets_id , ips__: ip   } , needs :   , tasks:{} },
+wallets_used_guide : { wallets_used:{ wallets_used_ids__: wallets_used_id , wallets_used_versions__: wallets_used_vresion , needs_:{} , tasks:{} },
+cryptos_used_guide : { cryptos_used: { cryptos_used_ids__:__used_     , needs_:{} , tasks_ :{} },
+d,s _used_guide : { db_used: { dbs_used_ids__: __used_ ,_used_:__used_version } , needs_:{}, tasks_ :{} },
+datas_udsed_guide : { datas_used: {} ,needs: {} , uis :{ui_used_id __, ui_used_version __ } } ,
+
+
+guides
+:{
+  _guide : { db_package_:{ the_db_object } ,tasks_:{} },
+checking_guide : { checking_package_:{ the_checking_package_and_coded_tasks } , tasks_: { } },
+ sendin_guide : {    e_ding _package_:{         _package_and_coded_tasks } , tasks_:{ } },
+ limits_guide : { needs_:{ } , limits_:{ } },
+rewards_guide : { needs_:{ } ,tasks_:{ } }}},},
+
+
+ checking_package
+
+
+send_backs_sorted
+ :{ asks_ids},asks_level_checks
+,levels:{
+ asks_level_  } _send_backs_level_   }   ,   approval   , send_backs_sorted  integer } ,
+ asks_level_  } , _send_backs_  level_sorted
+,level_   level__ send_backs_sorted
+asks_level_      sending_backs   }   ,level_  level_ _send_backs_sorted
+,,level__send_backs_sorted
+
+ } } }
+
+
+,. asking: {
+ prompt_user_for_instance_of: {
+
+
+
+
+
+
+   prompted  is = ' <button name="',(  .name.constructor  ) ,'"    ="',(  methods.prompt_  ,'" id="',(  this.name.constructor '"   ="' ) , '" > </button>'  .(  ) ;
+  };
+
+  for ( var instance_of_ of instance_of__ ) {
+
+  for ( ; instance_of_ . name .   ( -  1 ) = "_" ; ) {
+
+  prompted = + [ ' <p> '
+
+,   ' <div >'
+
+      ,(  instance_of_. name. constructor  );,
+
+
+, ' </div> ' ,
+, '=" ' <div >
+
+ ,(  instance_of_  )
+
+
+, ' </div> '
+
+,   ' <input type="radio"    =" "   > </p> ' ] .  ( ''); };
+
+for ( ; instance_of_ . name .   ( -  1 ) = "__" ; ) {
+
+prompted = + [ ' <p> '
+,(   instance_of_. name. constructor  ) ,
+ , '   '
+, ' <input type="text" value="" placeholder ="'
+,(  instanc1of_  ) ,
+, '" id= "'
+,
+(  instance_of_.    '" value="" > '
+, ' </p> '
+ ].join ( '' );
+}
+
+for ( ; instance_of_ . name .   ( -  1 ) != '__' ; )
+{
+
+)1 ,);,}} methods . prompt_ =
+
+ methods. click = ( 'instance_of__ = instance_of_ . constructor '));
+
+for( ;     ,     ("")   "_" ; ) {
+} ,
+
+
+
+prompted
+
+   =   '<button id="'
+, instance_of_.name
+, '" name="'
+,,instance_of_.name
+, '" value="'
+,, instance_of_.name
+, '" onclick="'
+,methods.prompt_
+ , '">'  ( '' );
+
+
+
+
+
+}  ,window.prompt
+= ( for (   = 0;1  ; ) {
+
+= }
+    ): ;
+
+} , sort_ips_that_the_capacity_allows_according_to_the_checking_ :{
+ file   {
+for (    = 0;   < count;  ++) {
+for
+  }
+}}},}redirecting_asks_at_ips:{
+, file {
+for (    = 0;   < count;  ++) { console  log
+( '="'  ssh'="''="'  porthere'="'ip remote
+();,, ( ) :
+();  ,(instance )  ;,},}}
+, methods.receiving_asks:{
+
+
+
+ redireting_received_asks_at_ips:{
+(;,, }
+
+, redireting_received_asks_at_files:{
+
+();,, }
+,,comparing_received_asks_and_dbs:{
+
+
+();,, comparing_for_duplicate_asks:{
+  );,, }
+,        comparing_for_constancy:{
+
+);,,);,,  ,receiving_send_backs:{
+}
+
+,,redirecting_received_send_backs_at_files  {
+
+
+
+, parse  file  title  {
+
+
+,  create
+
+   folder  { folder
+
+);, ,  , file  {
+
+,  wait  {
+   );
+
+, sending_backs  {
+
+
+
+  , redirecting_received_send_backs_at_ips  {
+
+, comparing_for_usual_constancy   {
+
+comparing_for_usual_constancy
+wallets are unlimited
+and the elctricity needed to unwanted decrypt for it should avoid it
+this should limit
+
+and it shuold be reencrypted or used
+,
+,
+.
+}
+1
