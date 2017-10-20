dca529f80934873fe6641e12a60d40d03c0ef72e
bitcoremgv <bitcoremgv@users.noreply.github.com>
2017-10-20 12:27:46 +0000

    Update thepackage.C

--- thepackage.C
+++ thepackage.C
@@ -9,17 +9,17 @@ var dbs = {
 
 
 
- asks_ids:{ place :{ place { latLng :{ lng   : lng, lat   : lat } , _place   : _place , place_  : place_ }} , current_ : current  ,low_id:{ is_low_id   ,
+ asks_ids:{ place :{ place { latLng :{ lng   : lng, lat   : lat } , _place   : _place , place_  : place _ }} , current_ : current  ,low_id:{ is_low_id   ,
   checkers_ids_: checkers_id , reencrypted_asks_ids : reencrypted_asks_id , asks_track_ : Number ( asks_track_integer ) ,
  asks_level_ : Number ( asks_level_integer ) , asks_letter_ : Number ( asks_letter_integer ) }}, 
  at_at_:{ Number ( at_at_integer ) :{_of_:{ Number ( of_at_integer ) :{ }}}} , 
 at :{ at_crypteds:{ Number ( at_integer ) :{_of_:{Number ( reencrypted_integer) :{ reencrypted_of_ :{Number ( needed_integer ) :{ needed_of_:{      ( crypteds_integer ) :{ used:{ :{ 
-  }}}}}}, off  _
-   }, wallets  on
- }}}, = (((())))) ' wallets_of_  off asked  wallets on asked
-wallets :{ wallets_on_  :{ wallets_ off_ids__ : wallets_off_id        has_checkers_used_  id : 
- :{  wallets_off_  instance_  id  that_has_ checkers  _id__ ids}}}:,
-,  is () () id not_the_  _off_ids__ :{    _ids__ : reencryption_asks_   ,  of_integer __ not_the_
+  }}}}}},   off place_
+   },   
+ }}}, = (((())))) ' wallets_    off asked  _of_   wallets on asked
+wallets :{wallets on   wallets_on_  :{ wallets_ off_ids__ : wallets_off_id        has_checkers_used_  id : 
+ :{  wallets_off_  instance_  id  that_has_ checkers  _id__ ids}}}:, __
+,  is () () id not_the_  _off_ids__ :{    _ids__ : reencryption_asks_   , of_integer  not_the_
 _at_    instance_of_ keys_for_the_binary_buffer , keys_for integer _id : _id reencrypted   asks_ids : is_reencryption_asks_ids_no   , }}} ,
   }}},:, {, , asks asks_level_  :is_asks_letter_place
   ledgers :{ _,
@@ -27,16 +27,16 @@ wallets_ledger:{ at_at_ , at_,
  crypteds , 
  asks_ids , 
        } , 
- place is place latLng lng  is  lng lat place_ curent_:{iscurrenr, 
+ place is place latLng lng  is  lng lat  curent_:{iscurrenr, 
  integer is needed  crypteds  asks track is  track  integer ,},:{ 
-  id asks level integer  asks letter 
+  id asks level integer asks letter 
   
  
  
  crypteds:{ 
-  decrypted__ : new String ( bunch_of_o_and_one ) , 
-  cryptos_used :{ to_crypted :{ cryptos_used_id_to_crypted__ : cryptos_used_id_to_crypted , crypted__ : crypted }facultative   semi  , cryptos_used_id_to_crypted_minus_ __
-:low_id, ,reencrypted_ is_place__is_lat:{ is_low_id_checkers_ _place    to_crypted_minus:{ Number ( to_crypted_minus_integer ) :{ cryptos_used_id_to_crypted_minus__ : cryptos_used_id_to_crypted_minus ,  _  _crypted__      ,  _semi_ is_ crypted }, 
+  decrypted__ : new String ( bunch_of_o_and_one ) , _place
+  cryptos_used :{ to_crypted :{ cryptos_used_id_to_crypted__ : cryptos_used_id_to_crypted , crypted__ : crypted } facultative  facultative   semi  , cryptos_used_id_to_crypted_minus_ __
+:low_id, ,reencrypted_ is_place__is_lat:{ is_low_id_checkers_     to_crypted_minus:{ Number ( to_crypted_minus_integer ) :{ cryptos_used_id_to_crypted_minus__ : cryptos_used_id_to_crypted_minus ,  _  _crypted__      ,  _semi_ is_ crypted }, 
 used _ id _     __ facultative semi
  decryption_  ask_keys_:{ new String ( sending_backs_keys) :{   
   reencrypted__: { reencrypted_cryptos_used__ : reencrypted_cryptos_used ,keys_for_the_crypted_not_the_reencrypted__: keys_for_the_crypted_not_the_reencrypted } } } },  :  cryptos    , lowdollars__:{} } ,
@@ -44,9 +44,9 @@ used _ id _     __ facultative semi
 :{
    __ _ledger
  checkers_checks :{ 
-   
+   _
  checks_asks :{ isplain : the_checks_  things_that_is , 
-   instanceofdb : {     ( checks_instance_of_dbs_integer )   :{ checks_instance_of_dbs } } }, 
+   instanceofdb : {     ( checks_instance_of_dbs_integer ) :{ checks_instance_of_dbs } } }, 
    checks_ids : checks_asks_id ,  
    checks : send_backs_sorted } , 
   
@@ -57,20 +57,20 @@ constancy_asks_ids_ : constancy_asks_id ,
 constancy_checks_ : send_backs_sorted ,  
 unconstancy_issue__ : issue , 
 constancy_asks :{ isplain__ : the_constancy_things_that_is , instanceofdbs:{ Number ( constancy_asks_  instance_of_dbs_integer ) :{ __ :{ constancy_asks_instance_of_dbs } } } },
-  constancy_asks       _
+  constancy_asks 
 rewards_asks_ledger : { rewards_asks_ids : rewards_asks_id , checkers : checkers_id , rewards_asked__ : Number ( checker_asked_rewards_integer ) },
 mean_asked_rewards :{ mean_asks_ask_ids : mean_asks_id , mean_asked_rewards : Number ( mean_asked_rewards_integer ) } } },     
  
  
- _integers
+ 
  used_guide :{  
 checkers_used_guide : {	checkers_used:{  _ _and_the _  :{ '="' } , checkers_ids__: checkers_id , material_characteristics__: material_charecharacteristics , wallets__: wallets_id , ips__: ip   } , needs :   , tasks:{} },
 wallets_used_guide : { wallets_used:{ wallets_used_ids__: wallets_used_id , wallets_used_versions__: wallets_used_  version } , needs_:{} , tasks:       },  
 cryptos_used_guide : { cryptos_used: { cryptos_used_ids__ : cryptos  _used_ id        cryptos_used_versions__ : cryptos_used_version   } , needs_:{} , tasks_ :     },     
       _used_guide   {      _used :{ dbs_used_ids__ : dbs_  used_id ,  _used_versions__ :     _used_version } , needs_:{} , tasks_ :          } ,  
-datas_udsed_guide : { datas_used:{    _used_ids__ : datas  _used_id ,  _used_  } , needs : {} , uis :{ ui_used_ids__ coded_tasks__    ui_used_id ,   _used_versions__     ui_used_version }} , }}}
-  guide guide  package  and  {} {} {} __idsids _
-(){ __ids { } { ) { _db _
+datas_udsed_guide : { datas_used:{    _used_ids__ : datas  _used_id , datas _used_  } , needs : {} , uis :{ ui_used_ids__ coded_tasks__    ui_used_id ,   _used_versions__     ui_used_version }} , }}}
+  guide guide  package  and {} {} __ _ 
+(){ __ids { } { ) _integers { _db _ {} 
 guides 
 :{ the_ _package_and_ asks_ids__  and_  coded_tasks__    }
     the_sending_    _guide : { db_package_: the_db_object , tasks_ : coded_tasks } , 
