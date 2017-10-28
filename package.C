32baacaeaea51c4bcef8a77ae03565d401406636
bitcoremgv <bitcoremgv@users.noreply.github.com>
2017-10-28 19:18:10 +0000

    Update package.C

--- package.C
+++ package.C
@@ -45,18 +45,18 @@ for ( ; methodes. value = 0 ; ) {
  console. log( [ that , " this could be ", this_could_be .this.methodes.name , this . constructor ]) ; } }} };
 
 
-methods  is   : {
-,methods . is_asks_ids :{ 
-objects : typeof that === 'object'
-, prototypeof_asks_ids : asks_ids:{ _. place  place { latLng :{ lng : lng, lat : lat } , _place : _place , place_ : place_ } , current_ : current_ ,low_id:{ is_low_id   ,
-  checkers_ids_: , reencrypted_asks_ids : reencrypted_asks_       , asks_track_ : asks_track_integer , 
-   asks_level_ : ; asks_level_integer , asks_letter_ :   asks_letter_integer    } ,  
-  asks_ids . prototype.isPrototypeOf ( that )
+methods  is  _ : 
+    
+objects    typeof that === 'object'
+,methods . is_asks_ids :{ prototypeof_asks_ids : asks_ids:{ . place  place { latLng :{ lng : lng, lat : lat } , _place : _place , place_ : place_ } , current_ : current_ ,low_id:{ is_low_id   ,
+   :  checkers_ids_: , reencrypted_asks_ids : reencrypted_asks_       , asks_track_ : asks_track_integer , 
+   asks_level_  asks_level_integer , asks_letter_ :   asks_letter_integer    } ,  
+,    : asks_ids . prototype.isPrototypeOf ( that )
 , eight_element : that.childElementCount =        } , 
-Number isInteger  , methods.is_asks_level_integer:{
- .() that positive_integer :     
-, over_minus_one : 
-, under_eleven :    >= that ,    :    (that)   } (),
+ ; Number  { isInteger    that    , methods.is_asks_level_integer:{
+positive_integer  :  
+, over_minus_one : .()
+, under_eleven :    >= that ,    :    (that)   } () ,
 , .. ,   childElementCount 
 , methods.is_at_integer :{ 
  '="'}(''
@@ -140,29 +140,29 @@ formats = {
 
  ledgers :{ 
 
-   
+ 
  
 wallets_ledger :{ 
  instance_of_checkers_used_guide_the_keys_for_the_ids 
- wallets  wallets _  ids__  wallets_id id: is_asks_ids , { 
-crypteds ids instance_of_wallets_id_that_has_checkers_id__ 
-_ids checkers   _id   at_ at_:{ is_at_integer :{ _of_ :{ is_reencrypted_integer :{ reencrypted_of_ :{ is_needed_integer :{ needed_of_ :{ is_crypteds_integer )  : {used: {}} :{:
+ wallets  wallets_id   ids__  wallets_id : id is asks _ids , { 
+crypteds ids instance_of_wallets_id _   _ has _ checkers_id__ 
+_ids        at_ at_:{ is_at_integer :{ _of_ :{ is_reencrypted_integer :{ reencrypted_of_ :{ is_needed_integer :{ needed_of_ :{ is_crypteds_integer )  : {used: {}} :{:
 crypteds:{ 
   cryptos_used : { 
    to_crypted : { cryptos_used_ids__    is_cryptos_used_id , crypted__ : is_binary_buffer } 
-   to_crypted_minus_:{ is_crypted_minus_integer :{ cryptos_used_ids__ : is_cryptos_used  id ,     crypted__ :     _ used _ ids } }}} } ,
-     that    {
-    decrypted__ : is_binary_buffer , 
+   to_crypted_minus_:{ is_crypted_minus_integer :{ cryptos_used_ids__ : is_cryptos_used_  id ,   _crypted__ :     _ used _ ids } }}} } ,
+     that    
+ {   decrypted__ : is_binary_buffer , 
  asks_keys_ :{ is_sending_backs_keys :{ cryptos
 ,  (__   reencrypted__: { reencrypted_cryptos_used__   is_reencrypted_cryptos_used 
   keys_for_the_crypted_not_the_reencrypted :{ low_id_ : is_low_id , random_id_ : is_random_id ,
  keys_for_the_crypted_not_the_reencrypted__ : is_binary_buffer } 
-  is_asks_ids    
+      
 
- } } } }} } }} }}}} ,  has ids }}} , lowdollars__: is_low_integer } }, 
-crypteds
-
-
+ } } } }} } }} }}}} ,    }}} , lowdollars__: is_low_integer } }, 
+crypteds  
+ids checks has
+is_asks_ids  
 
 
 ,   :{checks_level:{   
@@ -170,14 +170,14 @@ crypteds
   checkers_ids_: is_checkers_id , approval_ : is_approval } }}} ,
  levels :{ asks_level_: { is_asks_level_integer:{ sending_backs_level_ :{ is_sending_backs_level_integer :{
   level_ : is_asks_level_integer , approval_: is_approval }} }}} 
-}} checks ,   
+}}  ,   
  checkers_checks 
- checkers_ids : id _ checkers
+ checkers_ids : _id  checkers
   checks_asks: { 
- isplain : is_plain , instanceofdbs :{ is_checks_asks_instance_of_dbs_integer :{ __  is_checks_asks_instance_of_dbs }}}} ,
+ isplain : is_plain , instanceofdbs :{ is_checks_asks_instance_of_dbs_integer :{ is_checks_asks_instance_of_dbs }}}} ,
  checks_asks_ids : is_checks_asks_  asks_id , 
  checks : is_send_backs_sorted } , 
-unconstancy is isplain integer ids
+   __unconstancy isplain integer ids
  
 constancy_ledger :{ 
  
@@ -188,19 +188,19 @@ constancy_ledger :{
   instanceofdbs :{ is_unconstancy_issues_integer :{ __ :{ is_unconstancy_issues_instance_of_dbs }}}} , 
   
  constancy_asks :{ 
-  __   _constancy_asks_ _ _  __ : is_plain , 
-  instanceofdbs_ :{ is_constancy_asks_instance_of_dbs_integer :{ __ :{ is_constancy_asks_instance_of_dbs }}}} ,
+     _constancy_asks_ _ _ is __ : is_plain , 
+__  instanceofdbs_ :{ is_constancy_asks_instance_of_dbs_integer :{ __ :{ is_constancy_asks_instance_of_dbs }}}} ,
  __()__ _}}}}  _ constancy asks 
 rewards_asks_ledger :{ 
   rewards_asks_ids  : is_rewards_asks_id , 
   checkers  : is_checkers_id , 
-  rewards_asked__ : is_asked_rewards_integer , 
-  rewards_checks_ : is_send_backs_sorted } , 
+  rewards_asked __ : is_asked_rewards_integer , 
+  rewards_checks  : is_send_backs_sorted } , 
  
 
 
+_
 
-_used_version
 
 used_guides:{
  checkers_used_guide : {
@@ -214,21 +214,21 @@ used_guides:{
    wallets_used_versions : is_wallets_used_version } ,
   needs : is_plain ,
   tasks : is_plain } ,
-    { ledger ask   
- __
+   
+{ledger_ask_ _used_version
  cryptos_used_guide :{ 
   cryptos_used :{ 
    cryptos_used_ids__ : is_cryptos_used_id , cryptos_used_versions__ : is_    _used_version } ,
   needs__: is_plain ,
   tasks__: is_plain } ,  
 
-dbs_used_guide :{ __
+dbs_used_guide :{ 
  db_used :{   _used_ids__ : is_db_used_id ,  _used_versions__ : is_db_used_version } , 
  needs__ : is_plain , 
  tasks__: is_plain }, 
 datas_used_guide :{ 
-datas_used :{   _used_ids__ : is_  _used_id: ,  _used_versions__ : is_    _used_version } ,
- needs__: is_plain , 
+datas_used :{   _used_ids__ : is_  _used_id: ,  _used_versions__ : is_  _used_version } ,
+ needs__: is_plain , __
   uis_used :{ ui_used_ids__ :   _ui_used_id , ui_used_versions__:   _ui_used_version }}} ,
  __constancy_asks_used__asks constancy 
  
